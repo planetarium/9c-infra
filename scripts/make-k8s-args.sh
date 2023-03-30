5e04f43fbf18c4a5dd2a7e7cd691b4282db0551a
Atralupus <me@atralupus.com>
2023-03-30 16:56:20 +0900

    fix naming

--- scripts/make-k8s-args.sh
+++ scripts/make-k8s-args.sh
@@ -5,26 +5,26 @@ NETWORK="$1"
 HEADLESS_TAG="$2"
 DATAPROVIDER_TAG="$3"
 SEED_BRANCH="$4"
-MANUAL_CMD="$5"
+MANUAL_ARGS="$5"
 
 file_path="'9c-infra/$NETWORK/chart/values.yaml'"
 sources=""
 
-if [[ $MANUAL_CMD ]]; then
-    sources=$MANUAL_CMD
+if [[ "$MANUAL_ARGS" ]]; then
+    sources=$MANUAL_ARGS
 else
-    if [[ $HEADLESS_TAG ]]; then
+    if [[ "$HEADLESS_TAG" ]]; then
         sources="'ninechronicles-headless/from tag $HEADLESS_TAG' $sources"
     fi
 
-    if [[ $DATAPROVIDER_TAG ]]; then
+    if [[ "$DATAPROVIDER_TAG" ]]; then
         sources="'ninechronicles-dataprovider/from tag $DATAPROVIDER_TAG' $sources"
     fi
 
-    if [[ $SEED_BRANCH ]]; then
+    if [[ "$SEED_BRANCH" ]]; then
         sources="'libplanet-seed/from branch $SEED_BRANCH' $sources"
     fi
 fi
 
-K8S_CMD="$file_path $sources"
-echo "K8S_CMD=$K8S_CMD" >> $GITHUB_OUTPUT
+K8S_CMD_ARGS="$file_path $sources"
+echo "K8S_CMD_ARGS=$K8S_CMD_ARGS" >> $GITHUB_OUTPUT
