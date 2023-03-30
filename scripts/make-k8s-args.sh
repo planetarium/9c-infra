2e3b1a3295c347e33cc3fc9011df32c2903c7ef5
Atralupus <me@atralupus.com>
2023-03-30 22:21:19 +0900

    restore

--- scripts/make-k8s-args.sh
+++ scripts/make-k8s-args.sh
@@ -10,22 +10,22 @@ MANUAL_ARGS=$5
 file_path="\"9c-infra/$NETWORK/chart/values.yaml\""
 sources=""
 
-if [[ ! -z "$MANUAL_ARGS" ]]; then
+if [ "$MANUAL_ARGS" ]; then
     sources="$MANUAL_ARGS"
 else
-    if [[ ! -z "$HEADLESS_TAG" ]]; then
+    if [ "$HEADLESS_TAG" ]; then
         sources="\"ninechronicles-headless/from tag $HEADLESS_TAG\" $sources"
     fi
 
-    if [[ ! -z "$DATAPROVIDER_TAG" ]]; then
+    if [ "$DATAPROVIDER_TAG" ]; then
         sources="\"ninechronicles-dataprovider/from tag $DATAPROVIDER_TAG\" $sources"
     fi
 
-    if [[ ! -z "$SEED_BRANCH" ]]; then
+    if [ "$SEED_BRANCH" ]; then
         sources="\"libplanet-seed/from branch $SEED_BRANCH\" $sources"
     fi
 fi
 
 K8S_CMD_ARGS="$file_path $sources"
 echo $K8S_CMD_ARGS
-# echo "K8S_CMD_ARGS=$K8S_CMD_ARGS" >> $GITHUB_OUTPUT
+echo "K8S_CMD_ARGS=$K8S_CMD_ARGS" >> $GITHUB_OUTPUT
