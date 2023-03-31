74384ea9358de4373673b5acd53c66a53e62d41a
Atralupus <me@atralupus.com>
2023-03-31 16:34:39 +0900

    double quote

--- scripts/make-k8s-args.sh
+++ scripts/make-k8s-args.sh
@@ -7,22 +7,22 @@ DATAPROVIDER_TAG=$3
 SEED_BRANCH=$4
 MANUAL_ARGS=$5
 
-file_path="'9c-infra/$NETWORK/chart/values.yaml'"
+file_path='"'"9c-infra/$NETWORK/chart/values.yaml"'"'
 sources=""
 
 if [ "$MANUAL_ARGS" ]; then
     sources="$MANUAL_ARGS"
 else
     if [ "$HEADLESS_TAG" ]; then
-        sources="'ninechronicles-headless/from tag $HEADLESS_TAG' $sources"
+        sources=""'"'"ninechronicles-headless/from tag $HEADLESS_TAG"'"'" $sources"
     fi
 
     if [ "$DATAPROVIDER_TAG" ]; then
-        sources="'ninechronicles-dataprovider/from tag $DATAPROVIDER_TAG' $sources"
+        sources=""'"'"ninechronicles-dataprovider/from tag $DATAPROVIDER_TAG"'"'" $sources"
     fi
 
     if [ "$SEED_BRANCH" ]; then
-        sources="'libplanet-seed/from branch $SEED_BRANCH' $sources"
+        sources=""'"'"libplanet-seed/from tag $SEED_BRANCH"'"'" $sources"
     fi
 fi
 
