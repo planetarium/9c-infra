0a0a3e870a53cb59a313e82794ee1a80d537938f
sky1045 <ghdwlgud4545@gmail.com>
2023-04-19 17:13:13 +0900

    Add control plane logging for 9c-main-v2 cluster

--- 9c-main/terraform/eks.tf
+++ 9c-main/terraform/eks.tf
@@ -6,6 +6,8 @@ resource "aws_eks_cluster" "cluster" {
     subnet_ids = var.create_vpc ? [for subnet in aws_subnet.public : subnet.id] : var.public_subnet_ids
   }
 
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
   depends_on = [
     aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController,
