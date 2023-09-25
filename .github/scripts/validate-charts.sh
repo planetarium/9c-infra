7377722423e6c19e9fc264e4e407f028b497778b
Atralupus <me@atralupus.com>
2023-09-25 14:29:29 +0900

    fix path

--- .github/scripts/validate-charts.sh
+++ .github/scripts/validate-charts.sh
@@ -3,5 +3,5 @@ do
     chart_dir=$(dirname "$dir")
     echo "Validating $chart_dir..."
     helm lint "$chart_dir" || exit 1
-    helm template "$chart_dir" | kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" || exit 1
+    helm template "$chart_dir" | ./kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" || exit 1
 done
