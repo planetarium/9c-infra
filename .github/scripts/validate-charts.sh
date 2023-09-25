c5c2bc6e40af1c79009268172ebf8d2ae8d473a3
Atralupus <me@atralupus.com>
2023-09-25 14:28:22 +0900

    Fix lint

---
+++ .github/scripts/validate-charts.sh
@@ -0,0 +1,7 @@
+find . -type d -name templates -print | while read -r dir
+do
+    chart_dir=$(dirname "$dir")
+    echo "Validating $chart_dir..."
+    helm lint "$chart_dir" || exit 1
+    helm template "$chart_dir" | kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" || exit 1
+done
