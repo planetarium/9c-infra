c4793d62bf8e6e2f2dc001c168072b1a223162f8
Moreal <dev.moreal@gmail.com>
2023-10-06 00:51:37 +0900

    Use datreeio/CRDs-catalog for argocd schema

--- .github/scripts/validate-charts.sh
+++ .github/scripts/validate-charts.sh
@@ -3,5 +3,5 @@ do
     chart_dir=$(dirname "$dir")
     echo "Validating $chart_dir..."
     helm lint "$chart_dir" || exit 1
-    helm template "$chart_dir" | ./kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" || exit 1
+    helm template "$chart_dir" | ./kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' || exit 1
 done
