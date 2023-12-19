b8c4f5ed085ab732b4c3b9a878c8fadff75cbc98
moreal <dev.moreal@gmail.com>
2023-12-19 17:33:07 +0900

    Restrict additional properties

--- .github/scripts/validate-charts.sh
+++ .github/scripts/validate-charts.sh
@@ -3,7 +3,7 @@ do
     chart_dir=$(dirname "$dir")
     echo "Validating $chart_dir..."
     helm lint "$chart_dir" || exit 1
-    helm template "$chart_dir" | ./kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' || exit 1
+    helm template "$chart_dir" | ./kubeconform -strict -summary -skip "ExternalSecret,SecretStore,Secret" -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' || exit 1
 done
 
 # Check multiplanetary networks manually
@@ -13,6 +13,6 @@ for cluster in "${CLUSTERS[@]}"; do
     for network in "${NETWORKS[@]}"; do
         helm template --values "$cluster/multiplanetary/network/$network.yaml" \
             --values "$cluster/multiplanetary/network/general.yaml" \
-            "charts/all-in-one" | ./kubeconform -summary -skip "ExternalSecret,SecretStore,Secret" -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' || exit 1s
+            "charts/all-in-one" | ./kubeconform -strict -summary -skip "ExternalSecret,SecretStore,Secret" -schema-location default -schema-location 'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' || exit 1s
     done
 done
