0f2f7376fda39ec3cfb341fa8120f3642db123fa
Jiwon <me@atralupus.com>
2024-01-03 01:33:51 +0900

    Add dev cluster (#1195)
    * Add dev cluster
    
    * Update addon versions

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,118 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 3"
+    }
+  }
+  required_version = ">= 1.3.6"
+
+  backend "s3" {
+    bucket = "9c-tfstates"
+    key    = "eks/9c-dev-v2"
+    region = "us-east-2"
+  }
+}
+
+module "common" {
+  source = "../../modules/root"
+
+  name = "9c-dev-v2"
+
+  create_vpc = true
+
+  loki_bucket = "loki-9c-dev-v2.planetariumhq.com"
+
+  # new vpc
+  vpc_name       = "9c-dev"
+  vpc_cidr_block = "10.0.0.0/16"
+  public_subnets = {
+    "us-east-2a" = "10.0.0.0/20"
+    "us-east-2b" = "10.0.16.0/20"
+    "us-east-2c" = "10.0.32.0/20"
+  }
+  private_subnets = {
+    "us-east-2a" = "10.0.128.0/20"
+    "us-east-2b" = "10.0.144.0/20"
+    "us-east-2c" = "10.0.160.0/20"
+  }
+
+  addon_versions = {
+    "coredns"            = "v1.10.1-eksbuild.2"
+    "kube-proxy"         = "v1.28.1-eksbuild.1"
+    "vpc_cni"            = "v1.14.1-eksbuild.1"
+    "aws_ebs_csi_driver" = "v1.24.0-eksbuild.1"
+  }
+
+  # node group
+  node_groups = {
+    "odin-m5_l_2c" = {
+      instance_types    = ["m5.large"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 0
+      min_size          = 0
+      max_size          = 12
+    }
+
+    "odin-r7g_xl_2c" = {
+      instance_types    = ["r7g.xlarge"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 0
+      min_size          = 0
+      max_size          = 10
+      ami_type          = "AL2_ARM_64"
+    }
+
+    "odin-m7g_xl_2c" = {
+      instance_types    = ["m7g.xlarge"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 0
+      min_size          = 0
+      max_size          = 6
+      ami_type          = "AL2_ARM_64"
+    }
+
+    "odin-m7g_2xl_2c_test" = {
+      instance_types    = ["m7g.2xlarge"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 0
+      min_size          = 0
+      max_size          = 15
+      ami_type          = "AL2_ARM_64"
+      disk_size         = 50
+      taints = [{
+        key    = "dedicated"
+        value  = "remote-headless-test"
+        effect = "NO_SCHEDULE"
+      }]
+    }
+
+    "odin-m7g_2xl_2c_validator" = {
+      instance_types    = ["m7g.2xlarge"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 0
+      min_size          = 0
+      max_size          = 6
+      ami_type          = "AL2_ARM_64"
+      disk_size         = 50
+      taints = [{
+        key    = "dedicated"
+        value  = "validator-test"
+        effect = "NO_SCHEDULE"
+      }]
+    }
+
+    "general-t3_medium" = {
+      instance_types    = ["t3.medium"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "ON_DEMAND"
+      desired_size      = 1
+      min_size          = 0
+      max_size          = 1
+    }
+  }
+}
