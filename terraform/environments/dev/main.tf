7acabb2c163b3e1e41532fea2330116bd6b42256
Mizuki <mizuki@planetariumhq.com>
2024-01-18 15:03:08 +0900

    9c-dev(libplanet4-test): use ARM instance for data-provider

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -63,6 +63,16 @@ module "common" {
       max_size          = 20
     }
 
+    "general-m7g_xl_2c" = {
+      instance_types    = ["m7g.xlarge"]
+      availability_zone = "us-east-2c"
+      capacity_type     = "SPOT"
+      desired_size      = 1
+      min_size          = 0
+      max_size          = 15
+      ami_type          = "AL2_ARM_64"
+    }
+
     "general-m7g_2xl_2c" = {
       instance_types    = ["m7g.2xlarge"]
       availability_zone = "us-east-2c"
