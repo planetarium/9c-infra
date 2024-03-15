de4b823071ad55c4cda45ce551a2d9a2413b4fad
area363 <kidonseo@gmail.com>
2024-03-15 13:39:22 +0900

    fix config

--- scripts/app/config.py
+++ scripts/app/config.py
@@ -14,7 +14,9 @@ class Config(NamedTuple):
     # signer key address
     key_address: Optional[str] = None
     # slack token
-    key_address: Optional[str] = None
+    slack_token: Optional[str] = None
+    # slack channel
+    slack_channel: Optional[str] = None
 
     @classmethod
     def init(self):
@@ -28,7 +30,8 @@ class Config(NamedTuple):
         for v in [
             "KEY_PASSPHRASE",
             "KEY_ADDRESS",
-            "SLACK_TOKEN"
+            "SLACK_TOKEN",
+            "SLACK_CHANNEL"
         ]:
             try:
                 setattr(self, v.lower(), os.environ[v])
