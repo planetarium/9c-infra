d3d0fe681aaf54aaf8f7b26c1720940cab8c5184
area363 <kidonseo@gmail.com>
2024-04-18 17:51:55 +0900

    correctly use env variable in livenessprobe

--- charts/all-in-one/scripts/common/readiness_probe_jwt.sh
+++ charts/all-in-one/scripts/common/readiness_probe_jwt.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-if [ -z "$1" ]; then
+if [ -z "$JWT_TOKEN" ]; then
   echo "Token not provided"
   exit 1
 else
   preloaded="$(
     curl \
       -H 'Content-Type: application/json' \
-      -H "Authorization: Bearer $1" \
+      -H "Authorization: Bearer $JWT_TOKEN" \
       --data '{"query":"query{nodeStatus{preloadEnded}}"}' \
       http://localhost:80/graphql \
     | jq .data.nodeStatus.preloadEnded
